d3b78334ca47b76f4370ac0e42e461f947730727
Jokubas <jokubas.stankaitis@biomapas.com>
2021-06-21 15:57:16 +0300

    Vendor done

---
+++ api/vendor/prices/send/__init__.py
@@ -0,0 +1,2 @@
+import send_price
+sp = send_price.SendPrice().__init__()
--- api/vendor/prices/send/send_price.py
+++ api/vendor/prices/send/send_price.py
@@ -1,6 +1,9 @@
 import json
+from pprint import pprint
 from random import random
 from typing import Dict
+import socket
+import os
 
 import requests
 
@@ -12,31 +15,35 @@ class SendPrice:
         current = "AC" # "DC"
         base_price = 0.07
         customerDirection = "random IP goes here"
-        distance_to_customer = random() * 100 # Kilometers
+        port = "1010"
+        #distance_to_customer = random() * 100 # Kilometers
         kilometerCharge = self.__generateKmCharge(current) # Added % tax per 1000km distance
-        prices = self.__generatePrices(base_price, kilometerCharge, distance_to_customer)
+        prices = self.__generatePrices(base_price)
+        prices["priceIncreasePer1000km"] = kilometerCharge
+        prices["factoryLon"] = 43
+        prices["factoryLat"] = 42
+        #self.__sendPricesToCustomer(customerDirection,prices, port)
 
-        #self.__sendPricesToCustomer(customerDirection,prices)
-
-    def __generatePrices(self, base_price: float, kilometerCharge: float, distance_to_customer: float ) -> Dict[str, float]:
+    def __generatePrices(self, base_price: float ) -> Dict[str, float]:
         price_of_electricity = {}
-        for time in range(60*60*24):
+        for time in range(60*24):
             if time == 0:
-                price_of_electricity[str(time)] = self.__electricityPriceGenerator(time, base_price)+kilometerCharge*distance_to_customer
-            else:
-                price_of_electricity[str(time)] = self.__electricityPriceGenerator(time, price_of_electricity[ str(time-1) ])+kilometerCharge*distance_to_customer
+                price_of_electricity[str(time)] = self.__electricityPriceGenerator(time, base_price)
+            else :
+                pprint({self.__electricityPriceGenerator(time, price_of_electricity[ str(time-1) ]), time})
+                price_of_electricity[str(time)] = self.__electricityPriceGenerator(time, price_of_electricity[ str(time-1) ])
         return price_of_electricity
 
     def __electricityPriceGenerator(self, time: int, last_price: float) -> float:
         if time < 60*60*7:
             return last_price - last_price*0.000000793650794
-        elif 60 * 60 * 7 <= time < 60 * 60 * 16:
+        elif  60 * 7 <= time <  60 * 16:
             return last_price + last_price * 0.0000009259259
-        elif 60 * 60 * 16 <= time < 60 * 60 * 18:
+        elif  60 * 16 <= time <  60 * 18:
             return last_price + last_price * 0.0000208333333
-        elif 60 * 60 * 18 <= time < 60 * 60 * 20:
+        elif 60 * 18 <= time < 60 * 20:
             return last_price - last_price * 0.0000208333333
-        elif 60 * 60 * 18 <= time < 60 * 60 * 20:
+        elif 60 * 18 <= time <  60 * 20:
             return last_price - last_price * 0.0000027027027
 
     def __generateKmCharge(self, current: str) -> float:
@@ -46,5 +53,11 @@ class SendPrice:
             return .03/1000
         else:
             return 0
-    def __sendPricesToCustomer(self, direction: str, prices: Dict[str, float]):
-        response = requests.post(direction, json.dumps(prices))
+    def __sendPricesToCustomer(self, direction: str, prices: Dict[str, float], port: str):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((direction, port))
+        s.listen(10)
+        c, addr = s.accept()
+        f = json.dumps(prices).encode()
+        c.send(f)
+        c.close()
